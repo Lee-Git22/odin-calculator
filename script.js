d34fb52cde0535844edb9abd1adefe855974a17b
Lee-Git22 <lee.alan9801@gmail.com>
2023-01-03 15:32:08 -0500

    Implement display and power button

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// Helper functions for operate
 function add(x, y) {
     return x + y;
 }
@@ -14,11 +16,6 @@ function divide(x, y) {
     return x / y;
 }
 
-// console.log(add(1,2));
-// console.log(subtract(1,2));
-// console.log(multiply(1,2));
-// console.log(divide(1,2));
-
 function operate(x, y, operator) {
     switch (operator != null) {
         case (operator == "+"):
@@ -31,11 +28,46 @@ function operate(x, y, operator) {
             return divide(x, y);
         default:
             console.log("invalid operator")
-            break;
     }
 }
 
-// console.log(operate(15, 14, "+"))
-// console.log(operate(15, 14, "-"))
-// console.log(operate(15, 14, "*"))
-// console.log(operate(15, 14, "/"))
+let power = false;
+
+num1 = ""
+num2 = ""
+
+// TODO: Populate display with numbers buttons
+const display = document.querySelector("#display")
+
+const numberButtons = document.querySelectorAll(".numbers");
+numberButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (power) {
+            num1 = num1 + button.textContent
+            display.textContent = num1;
+        }
+    })
+});
+
+
+
+// TODO: Implement functions buttons
+
+const functionButtons = document.querySelectorAll(".function");
+functionButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+
+        if (button.id == "powerButton") {
+            num1 = "";
+            power = !power
+            if (power) {
+                display.textContent = "0";
+            } else {
+                display.textContent = "";
+            }
+
+        }
+
+
+    })
+})
