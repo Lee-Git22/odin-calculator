83be7db354a15c233d925df7b8e5fc5bb51e68d6
Lee-Git22 <lee.alan9801@gmail.com>
2023-01-04 16:25:05 -0500

    Full basic feature calculator done

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 
 // Helper functions for operate
 function add(x, y) {
-    return x + y;
+    return x + y
 }
 
 function subtract(x, y) {
-    return x - y;
+    return x - y
 }
 
 function multiply(x, y) {
-    return x * y;
+    return x * y
 }
 
 function divide(x, y) {
-    return x / y;
+    return x / y
 }
 
 function operate(x, y, operator) {
+    // console.log(x)
+    // console.log(y)
+    // console.log(operator)
+
     switch (operator != null) {
         case (operator == "add"):
             return add(x, y);
@@ -27,7 +31,7 @@ function operate(x, y, operator) {
         case (operator == "divide"):
             return divide(x, y);
         default:
-            console.log("invalid operator")
+            return "0" // TODO: return history
     }
 }
 
@@ -90,8 +94,12 @@ functionButtons.forEach((button) => {
                 break
 
             case (button.id == "back" && power): // Add special logic for result
-                display.textContent = String(display.textContent)
-                display.textContent = display.textContent.substring(0, display.textContent.length - 1)
+                if (display.textContent != result) {
+                    display.textContent = String(display.textContent)
+                    display.textContent = display.textContent.substring(0, display.textContent.length - 1)
+                } else {
+                    // TODO: clear history
+                }
                 break
 
             case (button.id == "negate" && power):
@@ -99,16 +107,44 @@ functionButtons.forEach((button) => {
                 break
             
             case (button.id == "operate" && power):
-                result = operate(parseInt(num1), parseInt(num2), operator)
+                result = operate(parseFloat(num1), parseFloat(num2), operator)
                 num1 = ""
                 num2 = ""
                 display.textContent = result
                 switchDisplay = false
                 break
             
+            case (button.id == "float" && power):
+                // Adds a decimal onto display
+                display.textContent = display.textContent + "."
+
+                // Adds a decimal to value
+                if (switchDisplay) {
+                    num2 = num2 + "."
+                    console.log("added")
+                } else {
+                    num1 = num1 + "."
+                    console.log("added")
+
+                }
+                break
+            // Default case assumes only num1 input 
             case (button.id == "add" || button.id == "subtract" || button.id == "multiply" || button.id == "divide" && power):
+
+                // If there are inputs for both numbers, evaluate first as num1 and clear num2
+                if (num1 != "" && num2 != "") {
+                    num1 = operate(parseInt(num1), parseInt(num2), operator)
+                    num2 = ""
+                    display.textContent = num1
+
+                // If there are no inputs(occurs after using operate), set num1 to result 
+                } else if (num1 == "" && num2 == "" && result != "") {
+                    num1 = result
+                }
+
+                // Pass on the operator and switch display
                 operator = button.id
-                switchDisplay = !switchDisplay
+                switchDisplay = true
                 break
             
 
